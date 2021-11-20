9f002a422259535a02293511d872af22387be0a2
Poscan <doodark13@gmail.com>
2021-11-21 01:00:32 +0500

    [+] Добавлен сервис для работы с пользователями

--- EasyLifeForHR/AdminPanel/src/store/index.ts
+++ EasyLifeForHR/AdminPanel/src/store/index.ts
@@ -1,11 +1,25 @@
+import axios from "axios";
 import Vue from "vue";
 import Vuex from "vuex";
+import User, { IUser } from "./model/user";
 
 Vue.use(Vuex);
+
+export class SuperPuperDuperService {
+  async GetUsers(): Promise<User[]> {
+    return (await axios.get("http://192.168.43.210:5000/User/api")).data.map((x: IUser) => new User(x));
+  }
+
+  async Authorize(login: string, password: string): Promise<number> {
+    return (await axios.get(`http://192.168.43.210:5000/Auth/api?login=${login}&password=${password}`));
+  }
+}
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
+  actions: {
+    
+  },
   modules: {},
 });
